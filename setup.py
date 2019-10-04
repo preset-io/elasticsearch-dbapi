163bfad0a645650aa469ac80b83cf1df598ebddd
Daniel Gaspar <danielvazgaspar@gmail.com>
2019-10-04 08:59:24 +0100

    Add tox and travis

--- setup.py
+++ setup.py
@@ -2,46 +2,37 @@ import os
 
 from setuptools import find_packages, setup
 
-VERSION = '0.1.0'
+VERSION = "0.1.0"
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 setup(
-    name='es-dbapi',
-    description=('DBAPI for Elasticsearch'),
-    long_description_content_type='text/markdown',
+    name="es-dbapi",
+    description=("DBAPI for Elasticsearch"),
+    long_description_content_type="text/markdown",
     version=VERSION,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     entry_points={
-        'sqlalchemy.dialects': [
-            'es = es.elastic.sqlalchemy:ESHTTPDialect',
-            'es.http = es.elastic.sqlalchemy:ESHTTPDialect',
-            'es.https = es.elastic.sqlalchemy:ESHTTPSDialect',
-            'esaws = es.aws.sqlalchemy:ESHTTPDialect',
-            'esaws.http = es.aws.sqlalchemy:ESHTTPDialect',
-            'esaws.https = es.aws.sqlalchemy:ESHTTPSDialect',
-        ],
+        "sqlalchemy.dialects": [
+            "es = es.elastic.sqlalchemy:ESHTTPDialect",
+            "es.http = es.elastic.sqlalchemy:ESHTTPDialect",
+            "es.https = es.elastic.sqlalchemy:ESHTTPSDialect",
+            "esaws = es.aws.sqlalchemy:ESHTTPDialect",
+            "esaws.http = es.aws.sqlalchemy:ESHTTPDialect",
+            "esaws.https = es.aws.sqlalchemy:ESHTTPSDialect",
+        ]
     },
-    install_requires=[
-        "elasticsearch>7",
-        "sqlalchemy",
-    ],
-    extras_require={
-        "aws": ["requests_aws4auth"],
-    },
-    author='Preset Inc.',
-    author_email='danielvazgaspar@gmail.com',
-    url='http://preset.io',
-    download_url=(
-        'https://github.com/preset-io/es-dbapi/' + VERSION,
-    ),
+    install_requires=["elasticsearch>7", "sqlalchemy"],
+    extras_require={"aws": ["requests_aws4auth"]},
+    author="Preset Inc.",
+    author_email="danielvazgaspar@gmail.com",
+    url="http://preset.io",
+    download_url=("https://github.com/preset-io/es-dbapi/" + VERSION,),
     classifiers=[
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    tests_require=[
-        'nose>=1.0',
-    ],
-    test_suite='nose.collector',
+    tests_require=["nose>=1.0"],
+    test_suite="nose.collector",
 )
--- utils/export.py
+++ utils/export.py
@@ -7,11 +7,8 @@ base_url = "http://localhost:9200"
 url = f"{base_url}/{index}/_search?size=100"
 file_path = "kibana_sample_data_flights.json"
 r = requests.get(url, headers=headers)
-data = [
-    doc["_source"]
-    for doc in r.json()["hits"]["hits"]
-]
+data = [doc["_source"] for doc in r.json()["hits"]["hits"]]
 
-fd = open(file_path, 'w')
+fd = open(file_path, "w")
 json.dump(data, fd)
 fd.close()
