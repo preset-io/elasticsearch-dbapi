81fbe45cb0000f50f6a4a804973046808c056f19
Daniel Gaspar <danielvazgaspar@gmail.com>
2020-01-04 13:22:48 +0000

    [dbapi] Improve support con string without trailing slash

--- es/tests/test_dbapi.py
+++ es/tests/test_dbapi.py
@@ -162,7 +162,7 @@ class TestData(unittest.TestCase):
         mock_elasticsearch.return_value = None
         connect(host="localhost", user="user", password="password")
         mock_elasticsearch.assert_called_once_with(
-            "http://localhost:9200", http_auth=("user", "password")
+            "http://localhost:9200/", http_auth=("user", "password")
         )
 
     @patch("elasticsearch.Elasticsearch.__init__")
@@ -173,5 +173,5 @@ class TestData(unittest.TestCase):
         mock_elasticsearch.return_value = None
         connect(host="localhost", user="user", password="password", scheme="https")
         mock_elasticsearch.assert_called_once_with(
-            "https://localhost:9200", http_auth=("user", "password")
+            "https://localhost:9200/", http_auth=("user", "password")
         )
