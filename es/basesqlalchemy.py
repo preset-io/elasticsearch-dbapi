a595d5fca69041f4d6c85efb359d000e338b7d26
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2021-01-20 09:36:57 +0000

    Update es/basesqlalchemy.py
    Co-authored-by: Ville Brofeldt <33317356+villebro@users.noreply.github.com>

--- es/basesqlalchemy.py
+++ es/basesqlalchemy.py
@@ -38,7 +38,7 @@ class BaseESCompiler(compiler.SQLCompiler):
 
 
 class BaseESTypeCompiler(compiler.GenericTypeCompiler):
-    def visit_REAL(self, type_, **kwargs: Dict[str, Any]) -> str:
+    def visit_REAL(self, type_, **kwargs: Any) -> str:
         return "DOUBLE"
 
     def visit_NUMERIC(self, type_, **kwargs: Dict[str, Any]) -> str:
