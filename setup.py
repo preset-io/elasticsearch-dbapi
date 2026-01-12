44305b805c9d09b7c255d62e89b4d7eb4ac051ba
zhaojinxin409 <5874804+zhaojinxin409@users.noreply.github.com>
2026-01-12 15:57:19 +0800

    bump elasticsearch version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
             "odelasticsearch.https = es.opendistro.sqlalchemy:ESHTTPSDialect",
         ]
     },
-    install_requires=["elasticsearch>7, <7.14", "packaging>=21.0", "sqlalchemy"],
+    install_requires=["elasticsearch>7, <8", "packaging>=21.0", "sqlalchemy"],
     extras_require={"opendistro": ["requests_aws4auth", "boto3"]},
     author="Preset Inc.",
     author_email="daniel@preset.io",
